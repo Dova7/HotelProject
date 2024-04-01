b85fb561bf9cb0c733fff45a2d1592e556d2da8f
George Gujarelidze <gujarelidze@gmail.com>
2024-04-02 02:14:50 +0400

    feature: nullable variables

--- HotelProject.Models/Room.cs
+++ HotelProject.Models/Room.cs
@@ -4,8 +4,8 @@
     {
         public int Id { get; set; }
         public string? RoomName { get; set; }
-        public bool IsBooked { get; set; }
-        public int HotelId { get; set; }
+        public bool? IsBooked { get; set; }
+        public int? HotelId { get; set; }
         public double? PriceGel { get; set; }
     }
 }
