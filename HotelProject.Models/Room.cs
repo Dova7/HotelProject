57988e1b06252ac6b31b497827bba56e6fffcc5f
George Gujarelidze <gujarelidze@gmail.com>
2024-04-02 01:50:40 +0400

    feature: added room model

---
+++ HotelProject.Models/Room.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelProject.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string? RoomName { get; set; }
+        public bool IsBooked { get; set; }
+        public int HotelId { get; set; }
+        public double? PriceGel { get; set; }
+    }
+}
