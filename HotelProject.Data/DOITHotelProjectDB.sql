1c2b2ea20c079f9bed8bbef697f2089489fab80b
George Gujarelidze <gujarelidze@gmail.com>
2024-04-01 22:59:23 +0400

    fix: DB structure

--- HotelProject.Data/DOITHotelProjectDB.sql
+++ HotelProject.Data/DOITHotelProjectDB.sql
@@ -1,13 +1,5 @@
 USE DOITHotel_BCTFO
 
-
-CREATE TABLE Managers
-(
-	Id INT PRIMARY KEY IDENTITY NOT NULL,
-	FirstName NVARCHAR(50) NOT NULL,
-	SecondName NVARCHAR(50) NOT NULL
-)
-
 CREATE TABLE Hotels
 (
 	Id INT PRIMARY KEY IDENTITY NOT NULL,
@@ -16,27 +8,32 @@ CREATE TABLE Hotels
 	Country NVARCHAR(50) NOT NULL,
 	City NVARCHAR(50) NOT NULL,
 	PhysicalAddress NVARCHAR(50) NOT NULL,
-	ManagerId INT FOREIGN KEY REFERENCES Managers(Id)
 )
 
-CREATE UNIQUE NONCLUSTERED INDEX UniqueManagerID
-ON DOITHotel_BCTFO.dbo.Hotels (ManagerId)
-WHERE ManagerId IS NOT NULL;
+CREATE TABLE Managers
+(
+	Id INT PRIMARY KEY IDENTITY NOT NULL,
+	FirstName NVARCHAR(50) NOT NULL,
+	SecondName NVARCHAR(50) NOT NULL,
+	HotelId INT UNIQUE FOREIGN KEY REFERENCES Hotels(Id)
+)
 
+CREATE UNIQUE NONCLUSTERED INDEX UniqueHotelID
+ON DOITHotel_BCTFO.dbo.Managers (HotelId)
+WHERE HotelId IS NOT NULL;
 
-INSERT INTO Managers(FirstName,SecondName)
+INSERT INTO Hotels(HotelName,Rating,Country,City,PhysicalAddress)
 VALUES
-(N'Giorgi',N'Gujarelidze'),
-(N'Saba',N'Gujarelidze'),
-(N'Levani',N'Gujarelidze'),
-(N'Nika',N'Gujarelidze')
+(N'Radisson Blu',N'4.5',N'Georgia',N'Tbilisi',N'Rose Revolution Square'),
+(N'The Biltmore',N'4',N'Georgia',N'Tbilisi',N'Shota Rustaveli Ave'),
+(N'Rooms Hotel',N'4.3',N'Georgia',N'Tbilisi',N'Merab Kostava Street')
 
-INSERT INTO Hotels(HotelName,Rating,Country,City,PhysicalAddress,ManagerId)
+INSERT INTO Managers(FirstName,SecondName,HotelId)
 VALUES
-(N'Radisson Blu',N'4.5',N'Georgia',N'Tbilisi',N'Rose Revolution Square',N'1'),
-(N'The Biltmore',N'4',N'Georgia',N'Tbilisi',N'Shota Rustaveli Ave',N'2'),
-(N'Rooms Hotel',N'4.3',N'Georgia',N'Tbilisi',N'Merab Kostava Street',N'3')
-
+(N'Giorgi',N'Gujarelidze',N'1'),
+(N'Saba',N'Gujarelidze',N'2'),
+(N'Levani',N'Gujarelidze',N'3'),
+(N'Nika',N'Gujarelidze',null)
 
 CREATE PROCEDURE GetAllManagers
 AS
@@ -44,6 +41,7 @@ BEGIN
 	SELECT [Id]
       ,[FirstName]
       ,[SecondName]
+	  ,[HotelId]
   FROM [DOITHotel_BCTFO].[dbo].[Managers]
 END
 
@@ -55,18 +53,18 @@ BEGIN
       ,[Rating]
       ,[Country]
       ,[City]
-      ,[PhysicalAddress]
-      ,[ManagerId]
+      ,[PhysicalAddress]      
   FROM [DOITHotel_BCTFO].[dbo].[Hotels]
 END
 
 CREATE PROCEDURE AddManager
 	@firstName NVARCHAR(50),
-	@secondName NVARCHAR(50)
+	@secondName NVARCHAR(50),
+	@hotelID INT
 AS
 BEGIN
-	INSERT INTO Managers(FirstName,SecondName)
-	VALUES(@firstName,@secondName)
+	INSERT INTO Managers(FirstName,SecondName,HotelID)
+	VALUES(@firstName,@secondName,@HotelID)
 END
 
 CREATE PROCEDURE AddHotel
@@ -74,23 +72,24 @@ CREATE PROCEDURE AddHotel
 	@Rating FLOAT,
 	@Country NVARCHAR(50),
 	@City NVARCHAR(50),
-	@PhysicalAddress NVARCHAR(50),
-	@ManagerId INT
+	@PhysicalAddress NVARCHAR(50)
 AS
 BEGIN
-	INSERT INTO Hotels(HotelName,Rating,Country,City,PhysicalAddress,ManagerId)
-	VALUES(@HotelName,@Rating,@Country,@City,@PhysicalAddress,@ManagerId)
+	INSERT INTO Hotels(HotelName,Rating,Country,City,PhysicalAddress)
+	VALUES(@HotelName,@Rating,@Country,@City,@PhysicalAddress)
 END
 
 CREATE PROCEDURE UpdateManager
 	@firstName NVARCHAR(50),
 	@secondName NVARCHAR(50),
-	@id int
+	@hotelID INT,
+	@id INT
 AS
 BEGIN
 UPDATE Managers
 	SET FirstName = @firstName,
-		SecondName = @secondName
+		SecondName = @secondName,
+		HotelId = @hotelID
 	WHERE Id = @id
 END
 
@@ -100,7 +99,6 @@ CREATE PROCEDURE UpdateHotel
 	@Country NVARCHAR(50),
 	@City NVARCHAR(50),
 	@PhysicalAddress NVARCHAR(50),
-	@ManagerId INT,
 	@id INT
 AS
 BEGIN
@@ -109,8 +107,7 @@ UPDATE Hotels
 		Rating = @Rating,
 		Country = @Country,
 		City = @City,
-		PhysicalAddress = @PhysicalAddress,
-		ManagerId = @ManagerId
+		PhysicalAddress = @PhysicalAddress
 	WHERE Id = @id
 END
 
